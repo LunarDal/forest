f1aac7508452867ba019017ee6e9caedcadde493
Web_moon <moonlee97@naver.com>
2026-04-15 13:03:24 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    // nav 설정
     $('.sub_mn').hide()
     $('.nav > ul > li').mouseenter(function(){
         $('.sub_mn').show().css({height:0}).animate({height:300})
@@ -6,4 +7,29 @@ $(document).ready(function(){
     $('.sub_mn').mouseleave(function(){
         $(this).css({height:300}).animate({height:0})
     })
+
+    // 메인슬라이드 설정
+    var slide = $('#slide > .slide_img > li')
+    var current = 0
+    var setIntervalId
+
+    timer()
+
+    function timer(){
+        setIntervalId = setInterval(function(){
+            var n = current + 1
+            if(n == 3){
+                n = 0
+            }
+            move(n)
+        }, 3000)
+    }
+    function move(i){
+        if(current == 3) return;
+        var currentEl = slide.eq(current)
+        var nextEl = slide.eq(i)
+        currentEl.css({left:0}).stop().animate({left:'-100%'}, 1000)
+        nextEl.css({left:'100%'}).stop().animate({left:0}, 1000)
+        current = i
+    }
 })
